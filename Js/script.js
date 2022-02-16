2cf7edbb1f2fea96a9b17626c78056d3c5560343
Rezaul909 <reza1381996@gmail.com>
2022-02-17 04:31:08 +0600

    save-btn-completed

--- Js/script.js
+++ Js/script.js
@@ -30,3 +30,29 @@ document.getElementById("calculate-btn").addEventListener('click',function(){
 
     // console.log(totalExpenses);
 });
+
+document.getElementById("save-btn").addEventListener("click", function(){
+    const savePercentId = document.getElementById("percent-input");
+    const savePercentage = parseFloat(savePercentId.value);
+
+    const incomeId= document.getElementById("income-input");
+    const income= parseFloat(incomeId.value);
+    
+    const balanceId= document.getElementById("balance");
+    const balance = parseFloat(balanceId.innerText);
+
+    const savingAmount = income * savePercentage /100;
+    const remainingBalance = balance - savingAmount;
+
+    if(isNaN(savePercentage) || savePercentage < 0){
+        alert("Invalid input. please give a number input.");
+    }
+    else if(savingAmount > balance){
+        alert("you can't save more than "+ balance);
+    }
+    else{
+        document.getElementById("saving-amount").innerText = savingAmount;
+        document.getElementById("remaining-balance").innerText = remainingBalance;
+    }
+    // console.log(balance);
+});
