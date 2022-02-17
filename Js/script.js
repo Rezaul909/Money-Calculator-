bc0d8623a9ad604e1b19833c050766fd88e239a6
Rezaul909 <reza1381996@gmail.com>
2022-02-17 20:39:08 +0600

    functions-error-massage-added

--- Js/script.js
+++ Js/script.js
@@ -6,7 +6,12 @@ function getInputFieldValue(inputId){
 }
 //Function for set innerText of totalExpenses, balance, savingAmount and remainingBalance:
 function setInnerText(textId , textValue){
-    document.getElementById(textId).innerText = textValue;
+    document.getElementById(textId).innerText = textValue.toFixed(2);
+}
+//error massage part : 
+function errorMassage(errorId , successId){
+    document.getElementById(errorId).style.display = "block";
+    document.getElementById(successId).style.display = "none";
 }
 //calculate buttons :=======================================================
 document.getElementById("calculate-btn").addEventListener('click',function(){
@@ -16,37 +21,42 @@ document.getElementById("calculate-btn").addEventListener('click',function(){
     const clothCost = getInputFieldValue("cloth-input");
 
     const totalExpenses = foodCost + rentCost + clothCost;
-    const balance = income - totalExpenses;
-    if( isNaN(income) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothCost) || income<0 ||foodCost<0 || rentCost<0 || clothCost<0){
-        alert("Invalid input. please give a number input.");
-    }
-    else if(totalExpenses>income){
-        alert("you expenses too much money!");
-    }
-    else{
+    const balance =income - totalExpenses;
+
+    if(income >=0 && foodCost >=0 && rentCost >=0 && clothCost>=0){
         setInnerText("total-expenses" , totalExpenses);
         setInnerText("balance" , balance);
+        document.getElementById("error").style.display = "none";
+        document.getElementById("success").style.display = "none";
+    }
+    else if(totalExpenses>income){
+        errorMassage("error" , "success");
+    }
+    else{
+        errorMassage("error" , "success");
     }
 });
-//save button calculation===============================================
+//save button calculation================================================
 document.getElementById("save-btn").addEventListener("click", function(){
     const savePercentage = getInputFieldValue("percent-input");
-    const income= getInputFieldValue("income-input");
+    const income = getInputFieldValue("income-input");
     
-    const balanceId= document.getElementById("balance");
+    const balanceId = document.getElementById("balance");
     const balance = parseFloat(balanceId.innerText);
     //savingAmount and remainingBalance calculation:
     const savingAmount = income * savePercentage / 100;
     const remainingBalance = balance - savingAmount;
     
     if(isNaN(savePercentage) || savePercentage < 0){
-        alert("Invalid input. please give a number input.");
+        errorMassage("error" , "success");
     }
     else if(savingAmount > balance){
-        alert("you can't save more than "+ balance);
+        errorMassage("error" , "success");
     }
     else{
         setInnerText("saving-amount" , savingAmount);
         setInnerText("remaining-balance" , remainingBalance);
+        document.getElementById("success").style.display = "block";
+        document.getElementById("error").style.display = "none";
     }
 });
